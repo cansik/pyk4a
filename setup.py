4c9731f7c60aff13fefffbba357f5695959ec47c
Florian <florian@bildspur.ch>
2022-02-03 20:14:27 +0100

    fixing wheel directly in github ci

--- setup.py
+++ setup.py
@@ -115,7 +115,7 @@ detect_and_insert_sdk_include_and_library_dirs(include_dirs, library_dirs)
 
 module = Extension('k4a_module',
                    sources=['pyk4a/pyk4a.cpp'],
-                   libraries=['k4a', 'k4arecord', 'depthengine'],
+                   libraries=['k4a', 'k4arecord'],
                    include_dirs=include_dirs,
                    library_dirs=library_dirs
                    )
